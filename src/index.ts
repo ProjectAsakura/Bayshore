fe854f0a0c15762a660a9516002c5c82abc4cc3f
Rin <ry00001@protonmail.com>
2022-07-11 08:38:33 +0100

    Add stuff

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
 // Bayshore - a Wangan Midnight Maximum Tune 6 private server.
 // Made with love by Luna, and part of Project Asakura.
 
-import {Express} from 'express';
+import express from 'express';
 import {PrismaClient} from '@prisma/client';
+import https from 'https';
+import fs from 'fs';
+
+const app = express();
+
+https.createServer({
+    key: fs.readFileSync('server_wangan.key'),
+    cert: fs.readFileSync('server_wangan.crt')
+}, app).listen(9002, () => {
+    console.log('Server listening on port 9002!');
+})
