3af1e12a11d5dcf8c16c1ed263562b9669049f64
ghkkk090 <108461408+ghkkk090@users.noreply.github.com>
2022-08-09 10:23:00 +0700

    Update game.ts

--- src/modules/game.ts
+++ src/modules/game.ts
@@ -2989,7 +2989,7 @@ export default class GameModule extends Module {
 				{
 					playedAt = time!.playedAt - 172800;
 				}
-				else if(time!.playedAt === 0){
+				else if(time!.playedAt === 0 || time!.playedAt < 1659805200){
 					playedAt = 1659805200;
 				}
 				ghostTrail = ghost_trails!.trail;
