0add6f57c9005411791ff480da9fab31d796cb9a
ghkkk090 <kyuubinamikaze8@gmail.com>
2022-09-11 16:33:45 +0700

    ghost level saving

--- src/modules/game.ts
+++ src/modules/game.ts
@@ -173,6 +173,7 @@ export default class GameModule extends Module {
 					windowSticker: body.car!.windowSticker!,
 					lastPlayedAt: timestamps,
 					regionId: body.car!.regionId!,
+					ghostLevel: body.car!.ghostLevel!,
 					rgStamp: common.sanitizeInputNotZero(body.rgResult?.rgStamp),
 					stampSheetCount: common.sanitizeInputNotZero(body.rgResult?.stampSheetCount)
 				}
