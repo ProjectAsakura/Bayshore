59b890f9b2f03a99b0190fe33043d58689963ee2
ghkkk090 <kyuubinamikaze8@gmail.com>
2022-09-12 19:59:40 +0700

    Update api.ts

--- src/api.ts
+++ src/api.ts
@@ -5,6 +5,7 @@ import { Module } from "./module";
 
 export default class ApiModule extends Module {
     register(app: Application): void {
+        
         app.use(express.urlencoded({
             type: '*/*',
             extended: true
@@ -17,6 +18,69 @@ export default class ApiModule extends Module {
 
 
         // API Get Requests
+        // Get Current Bayshore Version
+        app.get('/api/bayshore_version', async (req, res) => {
+            let message: any = {
+                error: null,
+                version: null
+            };
+
+            message.version = 'v1.0.0';
+
+            // Send the response to the client
+            res.send(message);
+        })
+
+        // Post Login
+        app.post('/api/login', async (req, res) => {
+
+            // Get the request body
+			let query = req.query;
+
+            // Message Response
+            let message: any = {
+                error: null,
+                user: null
+            };
+
+            // Get the user from the database
+			let user = await prisma.user.findFirst({
+				where: {
+					chipId: query.cardChipId?.toString(),
+					accessCode: query.accessCode?.toString()
+				},
+				include: {
+					cars: {
+						select: {
+							state: true,
+							gtWing: true,
+							lastPlayedPlace: true,
+                            carId: true, 
+                            name: true,
+                            defaultColor: true,
+                            visualModel: true, 
+                            level: true, 
+                            title: true, 
+                            regionId: true, 
+                        }
+					}
+				}
+			});
+            
+            if(user)
+            {
+                message.user = user;
+            }
+            else
+            {
+                message.error = 404
+            }
+
+            // Send the response to the client
+            res.send(message);
+        })
+
+
         // Get Current Competition Id
         app.get('/api/get_competition_id', async (req, res) => {
 
@@ -25,6 +89,7 @@ export default class ApiModule extends Module {
 
             // Message Response
             let message: any = {
+                error: null,
                 competitionId: 1 // default
             };
 
@@ -80,6 +145,7 @@ export default class ApiModule extends Module {
 
             // Message Response
             let message: any = {
+                error: null,
                 cars: [],
                 lastPlayedPlace: 'Bayshore'
             };
