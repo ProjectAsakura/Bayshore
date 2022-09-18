cc366455beeef4deaef71d7e2ac8149c71503b14
ghkkk090 <kyuubinamikaze8@gmail.com>
2022-09-18 13:01:12 +0700

    remove api (for now)

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,6 @@ import fs from 'fs';
 import bodyParser from 'body-parser';
 import AllnetModule from './allnet';
 import MuchaModule from './mucha';
-import ApiModule from './api';
 import { Config } from './config';
 import process from 'process';
 import * as Sentry from '@sentry/node';
@@ -36,7 +35,6 @@ const PORT_API = 9003;
 const app = express();
 const muchaApp = express();
 const allnetApp = express();
-const apiApp = express();
 
 app.use(bodyParser.raw({
     type: '*/*'
@@ -80,11 +78,6 @@ allnetApp.use((req, res, next) => {
     next()
 });
 
-/*apiApp.use((req, res, next) => {
-    console.log(timestamp+` [   API] ${req.method} ${req.url}`);
-    next()
-});*/
-
 // Get all of the files in the modules directory
 let dirs = fs.readdirSync('dist/modules');
 // Loop over the files
@@ -116,7 +109,6 @@ app.all('*', (req, res) => {
 // Register the ALL.NET / Mucha Server
 new AllnetModule().register(allnetApp);
 new MuchaModule().register(muchaApp);
-new ApiModule().register(apiApp);
 
 // Sentry is in use
 if (useSentry)
@@ -149,9 +141,4 @@ https.createServer({key, cert}, muchaApp).listen(PORT_MUCHA, '0.0.0.0', 511, () 
 // Create the game server
 https.createServer({key, cert}, app).listen(PORT_BNGI, '0.0.0.0', 511, () => {
     console.log(`Game server listening on port ${PORT_BNGI}!`);
-})
-
-// Create the API server
-http.createServer(apiApp).listen(PORT_API, '0.0.0.0', 4369, () => {
-    console.log(`API server listening on port ${PORT_API}!`);
 })
