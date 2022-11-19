d031ab1075f35fa980c005b6144649fb6fae60fd
Luna <ry00001@protonmail.com>
2022-11-19 17:30:00 +0000

    Switch OTel to GRPC

--- src/tracing.ts
+++ src/tracing.ts
@@ -3,7 +3,7 @@
 import * as opentelemetry from '@opentelemetry/sdk-node';
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
 import { diag, DiagConsoleLogger, DiagLogLevel } from '@opentelemetry/api';
-import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-grpc';
 import process from 'process';
 
 diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO);
