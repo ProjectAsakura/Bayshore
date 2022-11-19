49f14889b83d83eacf07e972ab27301903e0542c
Luna <ry00001@protonmail.com>
2022-11-19 17:39:35 +0000

    Add service name for tracing

--- src/tracing.ts
+++ src/tracing.ts
@@ -4,6 +4,8 @@ import * as opentelemetry from '@opentelemetry/sdk-node';
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
 import { diag, DiagConsoleLogger, DiagLogLevel } from '@opentelemetry/api';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-grpc';
+import { Resource } from '@opentelemetry/resources';
+import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
 import process from 'process';
 
 diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO);
@@ -12,6 +14,11 @@ const sdk = new opentelemetry.NodeSDK({
     traceExporter: new OTLPTraceExporter({
         url: process.env.OPENTELEMETRY_OTLP_URI,
         headers: {}
+    }),
+    resource: new Resource({
+        [SemanticResourceAttributes.SERVICE_NAME]: 'bayshore',
+        [SemanticResourceAttributes.DEPLOYMENT_ENVIRONMENT]:
+            process.env.NODE_ENV !== undefined ? process.env.NODE_ENV : 'development',
     }),
     instrumentations: [getNodeAutoInstrumentations()]
 });
