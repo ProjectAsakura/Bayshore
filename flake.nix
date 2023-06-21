b756adf88fac7bedd8f2775a222bc9e3a04be0e8
Luna Nightshade <accounts@rya.gay>
2023-06-21 18:44:26 +0200

    We need that

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
                     export PRISMA_MIGRATION_ENGINE_BINARY="${prisma-engines}/bin/migration-engine"
                     export PRISMA_QUERY_ENGINE_BINARY="${prisma-engines}/bin/query-engine"
                     export PRISMA_QUERY_ENGINE_LIBRARY="${prisma-engines}/lib/libquery_engine.node"
-                    #export PRISMA_INTROSPECTION_ENGINE_BINARY="${prisma-engines}/bin/introspection-engine"
+                    export PRISMA_INTROSPECTION_ENGINE_BINARY="${prisma-engines}/bin/introspection-engine"
                     export PRISMA_FMT_BINARY="${prisma-engines}/bin/prisma-fmt"
                     '';
                 in rec {
